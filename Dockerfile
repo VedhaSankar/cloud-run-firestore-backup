968e91e10e9d80c8593547bfd932ca609293b86d
VedhaSankar <vedhasankar26@gmail.com>
2022-02-01 05:11:50 +0530

    versions dont match or smn

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,35 @@
 FROM google/cloud-sdk:latest AS base
 
-RUN apt update
-RUN apt install nodejs npm -y
-RUN npm install -g firebase-tools
-
 ADD . ./app
 
 WORKDIR /app
+
+# RUN apt-get update
+
+# RUN apt-get upgrade -y
+
+# RUN apt install nodejs -y
+
+# RUN apt-get install npm -y
+
+# nvm installation
+SHELL ["/bin/bash", "--login", "-c"]
+
+# RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# RUN nvm install 8.10.0
+# RUN nvm use 8.10.0
+
+RUN apt install nodejs -y
+
+RUN apt-get install npm -y
+
+RUN node -v && npm -v
+
+RUN npm install -g firebase-tools
 
 ADD modern-diode-339415-b6e71cbc756a.json /app/
 
 RUN export GOOGLE_APPLICATION_CREDENTIALS="/app/modern-diode-339415-b6e71cbc756a.json"
 
-CMD [ "./backup.sh" ] 
+# CMD [ "./backup.sh" ] 
+RUN ./backup.sh
--- backup.sh
+++ backup.sh
@@ -11,10 +11,12 @@ PROJECT_ID=modern-diode-339415 # Replace with your project ID
 # export CLOUDSDK_CORE_PROJECT="/app/modern-diode-339415-b6e71cbc756a.json"
 export CLOUDSDK_CORE_PROJECT=$PROJECT_ID
 
-gcloud auth activate-service-account firebase-adminsdk-klheo@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-b6e71cbc756a.json
+export GOOGLE_APPLICATION_CREDENTIALS="/app/modern-diode-339415-firebase-adminsdk-l8t1h-9a1f830664.json"
 
-gcloud firestore export gs://$BUCKET_NAME/$FOLDER_NAME
+# gcloud auth activate-service-account firebase-adminsdk-klheo@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-b6e71cbc756a.json
 
-# firebase auth:export $FILE_NAME.json --format=JSON && \
-# gsutil -m cp -r $FILE_NAME.json gs://$BUCKET_NAME/$FOLDER_NAME/$FILE_NAME && \
-# rm $FILE_NAME.json
+# gcloud firestore export gs://$BUCKET_NAME/$FOLDER_NAME
+
+firebase auth:export $FILE_NAME.json --format=JSON && \
+gsutil -m cp -r $FILE_NAME.json gs://$BUCKET_NAME/$FOLDER_NAME/$FILE_NAME && \
+rm $FILE_NAME.json
