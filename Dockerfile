ef9a42ad97723fe8fe91a34cbe5e7e1167d76851
VedhaSankar <vedhasankar26@gmail.com>
2022-02-01 16:40:12 +0530

    works nowww

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
 FROM google/cloud-sdk:latest
 
-ADD . ./app
-
-WORKDIR /app
-
-# SHELL ["/bin/bash", "--login", "-c"]
-
 RUN apt update && apt-get update && apt install wget
-# RUN npm install -g firebase-tools
 
 RUN wget https://firebase.tools/bin/linux/latest
 
 RUN ls
 
+RUN mv latest firebase && chmod +x firebase && mv firebase /usr/local/bin/firebase 
+
+ADD . ./app
+
+WORKDIR /app
+
 ADD modern-diode-339415-b6e71cbc756a.json /app/
 
 RUN export GOOGLE_APPLICATION_CREDENTIALS="/app/modern-diode-339415-b6e71cbc756a.json"
 
-# CMD [ "./backup.sh" ] 
-RUN ./backup.sh
+CMD [ "./backup.sh" ] 
+# RUN ./backup.sh
--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ FOLDER_NAME=dtr-backup-$(date +%Y-%m-%d-%H-%M)
 
 FILE_NAME=dtr-auth-backup-$(date +%Y-%m-%d-%H-%M).json
 
-BUCKET_NAME=fries-firestore-backups
+BUCKET_NAME=fries-firestore-backups 
 
 PROJECT_ID=modern-diode-339415 # Replace with your project ID
 
@@ -12,12 +12,12 @@ export CLOUDSDK_CORE_PROJECT=$PROJECT_ID
 
 export GOOGLE_APPLICATION_CREDENTIALS="/app/modern-diode-339415-firebase-adminsdk-l8t1h-9a1f830664.json"
 
-# gcloud auth activate-service-account firebase-adminsdk-klheo@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-b6e71cbc756a.json
+gcloud auth activate-service-account firebase-adminsdk-klheo@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-b6e71cbc756a.json
 
-# gcloud firestore export gs://$BUCKET_NAME/$FOLDER_NAME
+gcloud firestore export gs://$BUCKET_NAME/$FOLDER_NAME
 
-gcloud auth activate-service-account firebase-adminsdk-l8t1h@modern-diode-339415.iam.gserviceaccount.comfirebase-adminsdk-l8t1h@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-firebase-adminsdk-l8t1h-9a1f830664.json
+gcloud auth activate-service-account firebase-adminsdk-l8t1h@modern-diode-339415.iam.gserviceaccount.com --key-file=/app/modern-diode-339415-firebase-adminsdk-l8t1h-9a1f830664.json
 
-firebase auth:export $FILE_NAME.json --format=JSON && \
+firebase auth:export $FILE_NAME.json --format=JSON --project $PROJECT_ID && \
 gsutil -m cp -r $FILE_NAME.json gs://$BUCKET_NAME/$FOLDER_NAME/$FILE_NAME && \
-rm $FILE_NAME.json
+rm $FILE_NAME.json 
